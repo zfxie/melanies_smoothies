06367fccbb17eb82e3898c9665fb1a1f2737d463
Allen <xiezhengfeng@gmail.com>
2024-10-09 11:57:08 -0400

    Update streamlit_app.py
    disable some code

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-pd_df = my_dataframe.to_pandas()
+# pd_df = my_dataframe.to_pandas()
                                                                      
 ingredients = st.multiselect(
     "Chose up to 5 ingredients:",
@@ -31,11 +31,11 @@ if ingredients:
     ingredients_string = ''
     for fruit_chosen in ingredients:
         ingredients_string += fruit_chosen + ' '
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        url = "https://fruityvice.com/api/fruit/" + search_on
-        st.write('api url: ', url)
+        # search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        # st.subheader(fruit_chosen + ' Nutrition Information')
+        # url = "https://fruityvice.com/api/fruit/" + search_on
+        # st.write('api url: ', url)
         # fruityvice_response = requests.get(url)
         # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
